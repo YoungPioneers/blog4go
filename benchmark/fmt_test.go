8b227d623064e459c9376e0e7e80654a73ed5780
hjweddie <hjweddie@163.com>
2015-11-28 18:40:33 +0800

    fmt timecache benchmark

--- benchmark/fmt_test.go
+++ benchmark/fmt_test.go
@@ -8,7 +8,13 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 )
+
+type timeFormatCacheType struct {
+	now    time.Time
+	format string
+}
 
 func BenchmarkFmtFormat(b *testing.B) {
 	b.StopTimer()
@@ -21,6 +27,33 @@ func BenchmarkFmtFormat(b *testing.B) {
 	b.StartTimer()
 
 	for i := 0; i < b.N; i++ {
-		writer.WriteString(fmt.Sprintf("haha %s. en\\en, always %d and %.4f", "eddie", 18, 3.1415))
+		writer.WriteString(fmt.Sprintf("%s [%s]haha %s. en\\en, always %d and %.4f", time.Now().Format("2006-01-02 15:04:05"), "DEBUG", "eddie", 18, 3.1415))
+	}
+}
+
+func BenchmarkFmtWithTimecacheFormat(b *testing.B) {
+	b.StopTimer()
+	now := time.Now()
+
+	timeCache := timeFormatCacheType{
+		now:    now,
+		format: now.Format("2006-01-02 15:04:05"),
+	}
+
+	file, err := os.OpenFile("output.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.FileMode(0644))
+	if nil != err {
+		fmt.Println(err.Error())
+	}
+	writer := bufio.NewWriterSize(file, 4096)
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		now := time.Now()
+		if now != timeCache.now {
+			timeCache.now = now
+			timeCache.format = now.Format("[2006/01/02:15:04:05]")
+		}
+		writer.WriteString(fmt.Sprintf("%s [%s]haha %s. en\\en, always %d and %.4f", timeCache.format, "DEBUG", "eddie", 18, 3.1415))
 	}
 }
