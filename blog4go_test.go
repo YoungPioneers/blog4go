0ad3bd6ec369a93d65bcf1b31e313b6e5d43fac6
hjweddie <hjweddie@163.com>
2015-11-28 18:19:15 +0800

    统一格式化

--- blog4go_test.go
+++ blog4go_test.go
@@ -14,6 +14,6 @@ func BenchmarkFormat(b *testing.B) {
 	b.StartTimer()
 
 	for i := 0; i < b.N; i++ {
-		writer.Debugf("haha %s. en\\en, always %d and %f", "eddie", 18, 3.1415)
+		writer.Debugf("haha %s. en\\en, always %d and %.4f", "eddie", 18, 3.1415)
 	}
 }
