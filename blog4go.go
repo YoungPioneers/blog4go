e053a5eb2919f59a9ce37a87437bf8b98f1bb696
huangjunwei <huangjunwei@youmi.net>
2023-06-04 00:30:56 +0800

    fix initialized errors

--- blog4go.go
+++ blog4go.go
@@ -30,7 +30,7 @@ const (
 
 var (
 	// blog is the singleton instance use for blog.write/writef
-	blog Writer = &DefaultWriter{}
+	blog Writer
 
 	// global mutex log used for singlton
 	singltonLock *sync.RWMutex
